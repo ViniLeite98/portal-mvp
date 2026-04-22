1e1b05cb557e215295b636332915d97a8479da14
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-04-22 16:31:29 -0300

    Delete assets/js/data.js

--- assets/js/data.js
+++
@@ -1,9 +0,0 @@
-<script>
-function getFaltas() {
-  return JSON.parse(localStorage.getItem("faltas") || "[]");
-}
-
-function saveFaltas(faltas) {
-  localStorage.setItem("faltas", JSON.stringify(faltas));
-}
-</script>

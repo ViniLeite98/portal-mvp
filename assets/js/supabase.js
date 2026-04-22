ab3bf42f427863f9d8c8b941436279c3fd9970ed
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-04-22 16:35:22 -0300

    Create supabase.js

---
+++ assets/js/supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL = "https://rymehgsoarjeecuwyhny.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bWVoZ3NvYXJqZWVjdXd5aG55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTc0ODUsImV4cCI6MjA4NjIzMzQ4NX0.K9BUapByrh6TsSi5jEkL8HGLDmMT_tGSSykl7LGxIec";
+
+// ✅ nome correto
+const client = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+// opcional (recomendado)
+window.client = client;

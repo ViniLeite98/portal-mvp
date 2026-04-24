e23b3cc94724344a726de006188eb9fcec93cc8e
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-04-24 10:39:29 -0300

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,11 +1,12 @@
-// assets/js/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const SUPABASE_URL = "https://rymehgsoarjeecuwyhny.supabase.co";
 
-const SUPABASE_URL = "https://rymehgsoarjeecuwyhny.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bWVoZ3NvYXJqZWVjdXd5aG55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTc0ODUsImV4cCI6MjA4NjIzMzQ4NX0.K9BUapByrh6TsSi5jEkL8HGLDmMT_tGSSykl7LGxIec";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bWVoZ3NvYXJqZWVjdXd5aG55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NTc0ODUsImV4cCI6MjA4NjIzMzQ4NX0.K9BUapByrh6TsSi5jEkL8HGLDmMT_tGSSykl7LGxIec"
-
-export const supabase = createClient(
+// ✅ nome correto
+const client = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
-)
+);
+
+// opcional (recomendado)
+window.client = client;

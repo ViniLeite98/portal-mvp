96c81b690e153d551f6e0de6395f9d4563a1ce01
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-04-24 10:48:15 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,21 @@
 
+// assets/js/api.js
+
+export async function getTotalClientes() {
+  const { count, error } = await client
+    .from("clientes")
+    .select("*", { count: "exact", head: true })
+
+  if (error) throw error
+  return count
+}
+
+export async function getServicosAtivos() {
+  const { data, error } = await client
+    .from("servicos")
+    .select("*")
+    .ilike("status", "ativo")
+
+  if (error) throw error
+  return data
+}
